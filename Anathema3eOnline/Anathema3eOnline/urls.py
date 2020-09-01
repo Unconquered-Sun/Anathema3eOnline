0893fa5190855b9008ac58a5bc9455e7aff07974
selpathor <mcrichton262@gmail.com>
2020-09-01 17:40:49 -0400

    Fixed Urls again

--- Anathema3eOnline/Anathema3eOnline/urls.py
+++ Anathema3eOnline/Anathema3eOnline/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path(r'^', include("anathemaapp.urls")),
-    path(r'^admin/', admin.site.urls)
+    path('anathema/', include("anathemaapp.urls")),
+    path('admin/', admin.site.urls)
 ]
