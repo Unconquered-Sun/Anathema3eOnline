c7bf7dd06ab9cc7e5c2e95d5683697f5a9b22992
selpathor <mcrichton262@gmail.com>
2020-09-01 18:16:08 -0400

    Fixed wildcard URL in anathemaapp by moving it to the end of the list

--- Anathema3eOnline/anathemaapp/urls.py
+++ Anathema3eOnline/anathemaapp/urls.py
@@ -1,10 +1,11 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.conf.urls import include, url
 from django.contrib import admin
-from .views import Login
+from .views import Login, CreateUser
 
 
 urlpatterns = [
-	url('', Login.as_view() , name="Login"),
-	url('login/', Login.as_view() , name="Login")
+	url('login/', Login.as_view() , name="Login"),
+	url('createuser/', CreateUser.as_view() , name="CreateUser"),
+	url('', Login.as_view() , name="Login")
 ]
