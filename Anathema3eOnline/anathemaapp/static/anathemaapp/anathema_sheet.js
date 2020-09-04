b59cae08bdfa6f595d2cccbd0347097a70321c98
selpathor <mcrichton262@gmail.com>
2020-09-04 16:49:39 -0400

    Started loading tabData. Note: do not use the same iterator name across multiple functions in JS.

--- Anathema3eOnline/anathemaapp/static/anathemaapp/anathema_sheet.js
+++ Anathema3eOnline/anathemaapp/static/anathemaapp/anathema_sheet.js
@@ -34,18 +34,68 @@ $(document).ready(function(){
 		$("#content").html("NO TAB CONTENT");
 	}
 
+	loadTab(currentTab);
 
 
+
+
+
+	//Load contents of the current Tab
+	function loadTab(tabName){
+		console.log("Start Tab Load")
+		//Check if tabName exists as a key in exalt_data
+		if( exalt_data.hasOwnProperty(tabName) ){
+			console.log("In Tab")
+			tabData = exalt_data[tabName];
+			console.log(tabData.length)
+			//Iterate through the tabData
+			for(j = 0; j<tabData.length; j++){
+				console.log("HTML #"+j);
+				// console.log(tabData[i])
+				//Begin by creating the content and appending it to the associated ID
+				tempHTML = loadHTML(tabData[j]["html"]);
+				htmlTarget = "#"+tabData[j]["targetID"];
+				$(htmlTarget).append(tempHTML)
+
+				//Check if the new HTML has any associated data
+				//data types:
+					//none: no data associated
+					//dots: data is shown in dots and will contain a value and maximum
+					//text: data is shown in a textbox and will contain a value
+					//textdots: data is shown in a textbox with dots after it. Contains a textValue, dotValue, and dotMaximum
+				if( tabData[j]["targetID"]["dataType"] == "none" ){
+
+				}
+				else if( tabData[j]["targetID"]["dataType"] == "dots" ){
+
+				}
+				else if( tabData[j]["targetID"]["dataType"] == "text" ){
+					
+				}
+				else if( tabData[j]["targetID"]["dataType"] == "textdots" ){
+					
+				}
+				else{
+					//SCREAMS GEOMETRICALLY
+				}
+			}
+		}
+		else{
+			console.log("NO TAB CONTENT");
+			$("#content").html("NO TAB CONTENT");
+		}
+
+	}
 
 	//Create a HTML statement based on the contents of the htmlJSON
 	function loadHTML(htmlJSONList){
 		//htmlOutput will contain the final HTML formed from the list
-		var htmlOutput = "";
+		htmlOutput = "";
 		//Process all htmlJSONs passed in the list
-		for(var i=0; i<htmlJSONList.length; i++){
-			var htmlJSON = htmlJSONList[i];
+		for(i=0; i<htmlJSONList.length; i++){
+			htmlJSON = htmlJSONList[i];
 			//create the opening of the tag
-			var tempHTML = "<"+htmlJSON["htmlType"]+" ";
+			tempHTML = "<"+htmlJSON["htmlType"]+" ";
 			// add any attributes to the tag
 			for(key in htmlJSON["htmlAttributes"]){
 				tempHTML = tempHTML+key+"="+htmlJSON["htmlAttributes"][key]+" "
