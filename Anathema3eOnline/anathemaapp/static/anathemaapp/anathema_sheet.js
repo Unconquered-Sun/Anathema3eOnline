597f8339535637fdeb4de53891ce1f82474ad092
selpathor <mcrichton262@gmail.com>
2020-09-04 15:44:25 -0400

    Added basic HTML processing to the js. Beginning more complex HTML and Data processing

--- Anathema3eOnline/anathemaapp/static/anathemaapp/anathema_sheet.js
+++ Anathema3eOnline/anathemaapp/static/anathemaapp/anathema_sheet.js
@@ -3,12 +3,47 @@ $(document).ready(function(){
 	
 	//Process JSON
 	var json_text = $("#exalt_data").attr("exalt_data");
-	console.log(json_text,"\n----------------------");
-
 	json_text = json_text.split("'").join('"');
-	console.log(json_text);
-	
+	//Parse JSON to a usable object
 	var exalt_json  = jQuery.parseJSON(json_text);
 	console.log(exalt_json);
 
+	//Begin sheet setup
+	var exalt_config = exalt_json.config
+	var exalt_data = exalt_json.data
+
+	var tabNames = exalt_config.tabs
+	var tabConfig = exalt_config.tabConfig
+
+	//Create Tab Buttons using the tabConfig
+	for(key in tabConfig){
+		if( tabConfig[key].hasOwnProperty("tabButton") ){
+			$("#tabs").append( loadHTML(tabConfig[key]["tabButton"]) );
+		}
+	}
+
+
+
+
+	//Create a HTML statement based on the contents of the htmlJSON
+	function loadHTML(htmlJSONList){
+		//htmlOutput will contain the final HTML formed from the list
+		var htmlOutput = "";
+		//Process all htmlJSONs passed in the list
+		for(var i=0; i<htmlJSONList.length; i++){
+			var htmlJSON = htmlJSONList[i];
+			//create the opening of the tag
+			var tempHTML = "<"+htmlJSON["htmlType"]+" ";
+			// add any attributes to the tag
+			for(key in htmlJSON["htmlAttributes"]){
+				tempHTML = tempHTML+key+"="+htmlJSON["htmlAttributes"][key]+" "
+			}
+			//close the tag, add its content, and the closing tag
+			tempHTML = tempHTML+">"+htmlJSON["htmlContent"]+"</"+htmlJSON["htmlType"]+">";
+			// add to htmlOutput
+			htmlOutput = htmlOutput+tempHTML;
+		}
+		//return the result
+		return htmlOutput;
+	}
 });
